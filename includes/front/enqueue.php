2dd8c37abd4d108a37f1819ab19d51c6045ef676
Jacqueline <sdjacqueline@gmail.com>
2023-10-05 17:16:24 +0200

    theme edits

---
+++ includes/front/enqueue.php
@@ -0,0 +1,11 @@
+<?php
+
+function ltf_enqueue() {
+  wp_register_style('ltf_font_rubik_and_pacifico', 'https://fonts.googleapis.com/css2?family=Pacifico&family=Rubik:wght@300;400;500;700&display=swap', [], null);
+  wp_register_style('ltf_bootstrap_icons', get_theme_file_uri('assets/bootstrap-icons/bootstrap-icons.css'));
+  wp_register_style('ltf_theme', get_theme_file_uri('assets/public/index.css'));
+
+  wp_enqueue_style('ltf_font_rubik_and_pacifico');
+  wp_enqueue_style('ltf_bootstrap_icons');
+  wp_enqueue_style('ltf_theme');
+}
